a9ebb3889ec70967dd4e8670c6afad405f08ba4c
Nick Abate <nick.abate@outlook.com>
2023-06-26 23:17:54 -0400

    profile endpoint

--- server.js
+++ server.js
@@ -89,21 +89,34 @@ app.post("/register", (req, res) => {
 
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-  if (!found) {
-    res.status(400).json("user not found");
-  }
+  // let found = false;
+  // database.users.forEach((user) => {
+  //   if (user.id === id) {
+  //     found = true;
+  //     return res.json(user);
+  //   }
+  // });
+  db.select("*")
+    .from("users")
+    .where({ id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("Not found");
+      }
+    })
+    .catch((err) => {
+      res.status(400).json("Error getting user");
+    });
+  // if (!found) {
+  //   res.status(400).json("user not found");
+  // }
 });
 
 app.put("/image", (req, res) => {
   const { id } = req.body;
-  console.log(id);
+  // console.log(id);
 
   let found = false;
   database.users.forEach((user) => {
