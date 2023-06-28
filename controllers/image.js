7236147d33e8e5f93498b2058fcf2d69c174dd67
Nick Abate <nick.abate@outlook.com>
2023-06-28 15:15:34 -0400

    add clarifai call on backend

--- controllers/image.js
+++ controllers/image.js
@@ -11,4 +11,41 @@ const handleImage = (req, res, db) => {
     .catch((err) => res.status(400).json("Unable to get info"));
 };
 
-module.exports = { handleImage };
+const handleApiCall = (req, res) => {
+  const IMAGE_URL = req.body.input;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: "clarifai",
+      app_id: "main",
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Key 6ee94d09e09e4254a440f10b9f58d8de",
+    },
+    body: raw,
+  };
+
+  fetch(
+    `https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`,
+    requestOptions
+  )
+    .then((data) => data.json())
+    .then((data) => res.json(data))
+    .catch((err) => res.status(400).json("Error calling API server"));
+};
+
+module.exports = { handleImage, handleApiCall };
--- server.js
+++ server.js
@@ -72,6 +72,10 @@ app.put("/image", (req, res) => {
   image.handleImage(req, res, db);
 });
 
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
+
 app.listen(8080, () => {
   console.log(`Server is running...`);
 });
